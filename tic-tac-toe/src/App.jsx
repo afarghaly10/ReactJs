47b51e8bf487627024b3d132150ee0e5dc6f0f48
afarghaly10 <sami.farghaly@gmail.com>
2024-10-10 21:11:44 -0400

    Leaderboard

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,13 +1,12 @@
 import Player from "./components/Player";
-import PLayer from "./components/Player";
 
 function App() {
 	return (
 		<main>
 			<div id="game-container">
 				<ol id="players">
-					<Player name="player 1" symbol="X" />
-					<Player name="player 2" symbol="O" />
+					<Player initialName="player 1" symbol="X" />
+					<Player initialName="player 2" symbol="O" />
 				</ol>
 				GAME BOARD
 			</div>
--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,35 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, props }) {
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const buttonName = isEditing ? "Save" : "Edit";
+
+	const handleEditClick = () => {
+		setIsEditing((editing) => !editing);
+	};
+
+	const handleChange = (event) => {
+		setPlayerName(event.target.value);
+	};
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{!isEditing ? (
+					<span className="player-name">{playerName}</span>
+				) : (
+					<input
+						type="text"
+						required
+						placeholder={playerName}
+						onChange={handleChange}
+					/>
+				)}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>{buttonName}</button>
 		</li>
 	);
 }
