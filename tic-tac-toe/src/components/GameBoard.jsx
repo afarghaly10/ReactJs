46311bf8035ba1d6946e1353a5255a4b44657ef8
afarghaly10 <sami.farghaly@gmail.com>
2024-10-10 21:33:20 -0400

    gameboard updating object state immutability

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
 ];
+
 export default function GameBoard() {
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	const handleSelectSquare = (rowIndex, cellIndex, symbol) => {
+		setGameBoard((prevGameBoard) => {
+			const updatedBoard = [...prevGameBoard.map((row) => [...row])];
+			updatedBoard[rowIndex][cellIndex] = "X";
+			return updatedBoard;
+		});
+	};
+
 	return (
 		<ol id="game-board">
-			{initialGameBoard.map((row, rowIndex) => (
+			{gameBoard.map((row, rowIndex) => (
 				<li key={rowIndex}>
 					<ol>
 						{row.map((cell, cellIndex) => (
 							<li key={cellIndex}>
-								<button>{cell}</button>
+								<button onClick={() => handleSelectSquare(rowIndex, cellIndex)}>
+									{cell}
+								</button>
 							</li>
 						))}
 					</ol>
