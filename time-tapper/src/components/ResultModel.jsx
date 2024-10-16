c3cc55ece7d5008c0a0df856b3e4a9016e1ceeea
afarghaly10 <sami.farghaly@gmail.com>
2024-10-16 17:03:51 -0400

    dim-background-on-modal-open(refForward)

--- time-tapper/src/components/ResultModel.jsx
+++ time-tapper/src/components/ResultModel.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { forwardRef } from 'react';
 
-export default function ResultModel({ result, targetTime }) {
+const ResultModal = forwardRef(function ResultModel(
+	{ result, targetTime },
+	ref
+) {
 	return (
-		<dialog className="result-modal" open>
+		<dialog ref={ref} className="result-modal">
 			<h2>You {result}: </h2>
 			<p>
 				The target time was <strong>{targetTime}</strong> seconds.
@@ -15,4 +18,6 @@ export default function ResultModel({ result, targetTime }) {
 			</form>
 		</dialog>
 	);
-}
+});
+
+export default ResultModal;
--- time-tapper/src/components/TimerChallenge.jsx
+++ time-tapper/src/components/TimerChallenge.jsx
@@ -5,10 +5,12 @@ export default function TimerChallenge({ title, targetTime }) {
 	const [timerExpired, setTimerExpired] = useState(false);
 	const [timerStarted, setTimerStarted] = useState(false);
 	let timer = useRef();
+	const dialog = useRef();
 
 	const handleStart = () => {
 		timer.current = setTimeout(() => {
 			setTimerExpired(true);
+			dialog.current.showModal();
 		}, 1000 * targetTime);
 
 		setTimerStarted(true);
@@ -20,12 +22,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
 	return (
 		<>
-			{timerExpired && (
-				<ResultModal
-					result={timerExpired ? 'failed' : 'success'}
-					targetTime={targetTime}
-				/>
-			)}
+			<ResultModal
+				ref={dialog}
+				result={timerExpired ? 'failed' : 'success'}
+				targetTime={targetTime}
+			/>
 			<section className="challenge">
 				<h2>{title}</h2>
 				<p className="challenge-time">
