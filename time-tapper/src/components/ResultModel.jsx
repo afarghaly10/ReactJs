8d7f306b2d1a2d37a15f2486bdd910c3530a2cc4
afarghaly10 <sami.farghaly@gmail.com>
2024-10-16 23:30:20 -0400

    comments

--- time-tapper/src/components/ResultModel.jsx
+++ time-tapper/src/components/ResultModel.jsx
@@ -5,6 +5,15 @@ const ResultModal = forwardRef(function ResultModel(
 	ref
 ) {
 	const dialog = useRef();
+
+	/*
+  expose the a method that can be called with the help of ref outside this component
+  the method is open() which will show work independently of the component
+  The import of useRef is used to create a reference to the dialog element.
+  A separate ref is needed for reaching out to the dialog element because the idea
+  is to detach the dialog element from any other component and make it a standalone
+  */
+
 	useImperativeHandle(ref, () => {
 		return {
 			open() {
@@ -12,6 +21,7 @@ const ResultModal = forwardRef(function ResultModel(
 			},
 		};
 	});
+
 	return (
 		<dialog ref={dialog} className="result-modal">
 			<h2>You {result}: </h2>
