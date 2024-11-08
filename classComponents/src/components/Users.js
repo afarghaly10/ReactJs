9161a566204f972e5b5e9a6971b9e71633848edc
afarghaly10 <sami.farghaly@gmail.com>
2024-11-08 13:34:33 -0500

    componentDidUpdate

--- classComponents/src/components/Users.js
+++ classComponents/src/components/Users.js
@@ -2,13 +2,6 @@ import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Samantha' },
-  { id: 'u2', name: 'Daniel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class Users extends Component {
 	constructor() {
     super();
@@ -24,7 +17,7 @@ class Users extends Component {
 	render() {
     const usersList = (
 			<ul>
-				{DUMMY_USERS.map((user) => (
+				{this.props.users.map((user) => (
 					<User key={user.id} name={user.name} />
 				))}
 			</ul>
