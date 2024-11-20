df80bd2535e7d144b6df6e5277a4760801215633
afarghaly10 <sami.farghaly@gmail.com>
2024-11-20 13:08:46 -0500

    input validation and validation Errors

--- authentication/frontend/src/components/AuthForm.js
+++ authentication/frontend/src/components/AuthForm.js
@@ -1,15 +1,33 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import {
+	Form,
+	Link,
+	useActionData,
+	useNavigation,
+	useSearchParams,
+} from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
+	const data = useActionData();
+	const navigation = useNavigation();
+
 	const [searchParams] = useSearchParams();
 	const isLogin = searchParams.get('mode') === 'login';
+	const isSubmitting = navigation.state === 'submitting';
 
 	return (
 		<>
 			<Form method="post" className={classes.form}>
 				<h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+				{data && data.errors && (
+					<ul>
+						{Object.values(data.errors).map((error) => (
+							<li key={error}>{error}</li>
+						))}
+					</ul>
+				)}
+				{data && data.message && <p>{data.message}</p>}
 				<p>
 					<label htmlFor="email">Email</label>
 					<input id="email" type="email" name="email" required />
@@ -22,7 +40,13 @@ function AuthForm() {
 					<Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
 						{isLogin ? 'Create new user' : 'Login'}
 					</Link>
-					{isLogin ? <button>Sign in</button> : <button>Sign up</button>}
+					{isLogin ? (
+						<button>Sign in</button>
+					) : (
+						<button disabled={isSubmitting}>
+							{isSubmitting ? `Signing up...` : `Sign up`}
+						</button>
+					)}
 				</div>
 			</Form>
 		</>
