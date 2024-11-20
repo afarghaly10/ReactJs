9c248fbe621eea81624e0952949f0bbc753b3a89
afarghaly10 <sami.farghaly@gmail.com>
2024-11-20 17:04:36 -0500

    auto logout pt.1

--- authentication/frontend/src/pages/Root.js
+++ authentication/frontend/src/pages/Root.js
@@ -1,19 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	// const navigation = useNavigation();
+	const token = useLoaderData();
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+	// porgrammatically submit a form
+	const submit = useSubmit();
+
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
+
+		setTimeout(() => {
+			submit(null, { action: '/logout', method: 'post' });
+		}, 3600000);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
