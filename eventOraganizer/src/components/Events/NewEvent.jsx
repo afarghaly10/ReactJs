658d2e7f4dc958fa30e80c5be542c0e240446310
afarghaly10 <sami.farghaly@gmail.com>
2024-12-05 01:05:55 -0500

    final

--- eventOraganizer/src/components/Events/NewEvent.jsx
+++ eventOraganizer/src/components/Events/NewEvent.jsx
@@ -1,54 +1,51 @@
-import { useMutation } from '@tanstack/react-query';
 import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
-import { createNewEvent, queryClient } from '../../util/http.js';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
 
 export default function NewEvent() {
-	const { mutate, isPending, isError, error } = useMutation({
-		mutationFn: createNewEvent,
-		onSuccess: () => {
-			queryClient.invalidateQueries({
-				queryKey: ['events'],
-				// exact: true,
-			});
-			navigate('/events');
-		},
-	});
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    },
+  });
 
-	function handleSubmit(formData) {
-		mutate({ event: formData });
-	}
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
-	return (
-		<Modal onClose={() => navigate('../')}>
-			<EventForm onSubmit={handleSubmit}>
-				{isPending && <p>Creating event...</p>}
-				{!isPending && (
-					<>
-						<Link to="../" className="button-text">
-							Cancel
-						</Link>
-						<button type="submit" className="button">
-							Create
-						</button>
-					</>
-				)}
-			</EventForm>
-			{isError && (
-				<ErrorBlock
-					title="An error occurred"
-					message={
-						error.info?.details ||
-						error.info?.message ||
-						'Failed to create event'
-					}
-				/>
-			)}
-		</Modal>
-	);
+  return (
+    <Modal onClose={() => navigate('../')}>
+      <EventForm onSubmit={handleSubmit}>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+      </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event"
+          message={
+            error.info?.message ||
+            'Failed to create event. Please check your inputs and try again later.'
+          }
+        />
+      )}
+    </Modal>
+  );
 }
