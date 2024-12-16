44704683d3953c3573a238f5d07223fd112ba4e9
afarghaly10 <sami.farghaly@gmail.com>
2024-12-16 16:21:57 -0500

    vairants

--- animation/src/components/Modal.jsx
+++ animation/src/components/Modal.jsx
@@ -5,9 +5,15 @@ export default function Modal({ title, children, onClose }) {
 		<>
 			<div className="backdrop" onClick={onClose} />
 			<motion.dialog
-				animate={{ y: 0, opacity: 1 }}
-				initial={{ y: 30, opacity: 0 }}
-				exit={{ y: 30, opacity: 0 }}
+				variants={{
+					hidden: { opacity: 0, y: 30 },
+					visible: { opacity: 1, y: 0 },
+					exit: { y: 30, opacity: 0 },
+				}}
+				animate="visible"
+				initial="hidden"
+				exit="exit"
+				transition={{ duration: 0.2 }}
 				open
 				className="modal"
 			>
