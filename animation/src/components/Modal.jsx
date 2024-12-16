a13ef70604232eb0eca2365a403d8cfb6cbf05cf
afarghaly10 <sami.farghaly@gmail.com>
2024-12-16 15:44:48 -0500

    button and modal animation

--- animation/src/components/Modal.jsx
+++ animation/src/components/Modal.jsx
@@ -1,14 +1,20 @@
+import { motion } from 'framer-motion';
 import { createPortal } from 'react-dom';
-
 export default function Modal({ title, children, onClose }) {
-  return createPortal(
-    <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
-        <h2>{title}</h2>
-        {children}
-      </dialog>
-    </>,
-    document.getElementById('modal')
-  );
+	return createPortal(
+		<>
+			<div className="backdrop" onClick={onClose} />
+			<motion.dialog
+				animate={{ y: 0, opacity: 1 }}
+				initial={{ y: 30, opacity: 0 }}
+				exit={{ y: 30, opacity: 0 }}
+				open
+				className="modal"
+			>
+				<h2>{title}</h2>
+				{children}
+			</motion.dialog>
+		</>,
+		document.getElementById('modal')
+	);
 }
